1cb474d0df732e07f63649a71a633606b9880853
edymarfg <edymar-fg10@hotmail.com>
2022-06-01 13:30:45 -0300

    first commit

---
+++ src/main/java/br/com/edymar/bibliotecapessoal/App.java
@@ -0,0 +1,10 @@
+package br.com.edymar.bibliotecapessoal;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
---
+++ src/test/java/br/com/edymar/bibliotecapessoal/AppTests.java
@@ -0,0 +1,22 @@
+package br.com.edymar.bibliotecapessoal;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTests {
+
+    @Test
+    void mainTest() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        final String[] args = { "Olá" };
+        App.main(args);
+        assertEquals("[Olá]" + System.lineSeparator(), outContent.toString());
+    }
+}
